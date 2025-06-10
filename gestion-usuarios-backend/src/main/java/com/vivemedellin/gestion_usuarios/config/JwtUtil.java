af96b385de08147ae526dc13573dbdc2155391f8
JestebanAB <jesteban.agudelo1@udea.edu.co>
2025-06-10 18:18:14 -0500

    fix: rename secretKey to follow naming convention

--- gestion-usuarios-backend/src/main/java/com/vivemedellin/gestion_usuarios/config/JwtUtil.java
+++ gestion-usuarios-backend/src/main/java/com/vivemedellin/gestion_usuarios/config/JwtUtil.java
@@ -17,20 +17,20 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "claveSuperSecreta";
+    private final String secretKey = "claveSuperSecreta";
 
     public String generateToken(String email) {
         return Jwts.builder()
             .setSubject(email)
             .setIssuedAt(new Date())
             .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 horas
-            .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+            .signWith(SignatureAlgorithm.HS256, secretKey)
             .compact();
     }
 
     public String extractEmail(String token) {
         return Jwts.parser()
-            .setSigningKey(SECRET_KEY)
+            .setSigningKey(secretKey)
             .parseClaimsJws(token)
             .getBody()
             .getSubject();
