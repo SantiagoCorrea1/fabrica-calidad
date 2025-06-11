b432cf2db6097c6407bac2ebb625019c1e50d49b
JestebanAB <jesteban.agudelo1@udea.edu.co>
2025-06-10 22:56:09 -0500

    unused import removed on authController

--- gestion-usuarios-backend/src/main/java/com/vivemedellin/gestion_usuarios/controller/AuthController.java
+++ gestion-usuarios-backend/src/main/java/com/vivemedellin/gestion_usuarios/controller/AuthController.java
@@ -20,7 +20,6 @@ import java.util.Optional;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
